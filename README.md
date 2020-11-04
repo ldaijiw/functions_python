df4f8c91026d01a2dc687363813b7e6d77696d94
ldaijiw <lwaltmann@spartaglobal>
2020-11-04 11:24:23 +0000

    started functions

---
+++ README.md
@@ -0,0 +1,14 @@
+# Python Functions
+
+## What are functions and why are they useful
+
+**D**on't **R**epeat **Y**ourself
+
+### How to create a function
+
+- Syntax uses 
+```
+def function_name():
+```
+
+### Function best practices
---
+++ functions.py
@@ -0,0 +1,18 @@
+# Create a greeting function
+
+def greeting():
+    print("Welcome on board")
+
+# function will only execute if it is called
+
+greeting()
+# calling the function will execute the code inside the fn
+
+# function with arguments
+
+def add(num1, num2):
+    print(num1+num2)
+    return num1 + num2
+
+
+add(1,2)
